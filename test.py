f90ab37dd6dbdbdaccf1fa0e0e6806e1f189fb69
PrimoCrypt <yhungdew@gmail.com>
2022-07-11 17:56:38 +0100

    next commit

--- test.py
+++ test.py
@@ -7,28 +7,25 @@ two = "2. Hello your name"
 programList = [one , two]
 
 error = print("this is an error")
-
-
-def calculator(selection):    
-    x = int(input("x: "))
-    sign = input("sign(* , + , - or /): ")
-    y = int(input("y: "))
-
-    if (sign == "+"):
-        print(f"result for {x} + {y} = {x + y}")
-    elif (sign == "-"):
-        print(f"result for {x} - {y} = {x - y}")
-    elif (sign == "*"):
-        print(f"result for {x} x {y} = {x * y}")
-    elif (sign == "*"):
-        print(f"result for {x} / {y} = {x / y}")
-    else:
-        print("Unrecognized input, please try again later")
-
 def appList(programList):
     for dispList in programList:
         return dispList
-
+appList
 selection = input(f"pick from these list{appList(programList)}: ")
 if (selection == "1"):
+    def calculator(selection):    
+        x = int(input("x: "))
+        sign = input("sign(* , + , - or /): ")
+        y = int(input("y: "))
+
+        if (sign == "+"):
+            print(f"result for {x} + {y} = {x + y}")
+        elif (sign == "-"):
+            print(f"result for {x} - {y} = {x - y}")
+        elif (sign == "*"):
+            print(f"result for {x} x {y} = {x * y}")
+        elif (sign == "*"):
+            print(f"result for {x} / {y} = {x / y}")
+        else:
+            print("Unrecognized input, please try again later")
     calculator
